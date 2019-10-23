df10362109973115b652c11f9faf02554b7554ad
legusie <przydasie99@gmail.com>
2019-10-23 19:02:57 +0200

    sroda wieczur 2

--- src/lab2.ml
+++ src/lab2.ml
@@ -10,18 +10,44 @@ let rec podziel xs =
     let rec podziel2 (xs, x1s, x2s) =
         match xs with
         | [] -> (reverse(x1s), reverse(x2s))
-        | h::t -> if (h mod 2 ) = 0 then podziel2(t, (h :: x1s), x2s)
+        | h::t -> if (h mod 2) = 0 then podziel2(t, (h :: x1s), x2s)
         else podziel2(t, x1s, (h :: x2s))
 in podziel2((xs,[],[]));;
 
-podziel [1;2;3;4];;
-let (x1,x2) = podziel [1;2;3;4;5;6;7];;
 
+
+let (x1,x2) = podziel [1;2;3;4;5;6;7];;
 open Printf
 let () = List.iter (printf "%d ") (x1);;
-print_string "\n";;
+print_string "   ";;
 let () = List.iter (printf "%d ") (x2);;
 print_string "\n";;
+
+
+let (x1,x2) = podziel [1];;
+open Printf
+let () = List.iter (printf "%d ") (x1);;
+print_string "   ";;
+let () = List.iter (printf "%d ") (x2);;
+print_string "\n";;
+
+
+let (x1,x2) = podziel [];;
+open Printf
+let () = List.iter (printf "%d ") (x1);;
+print_string "   ";;
+let () = List.iter (printf "%d ") (x2);;
+print_string "\n";;
+
+
+let (x1,x2) = podziel [2;3;9;1];;
+open Printf
+let () = List.iter (printf "%d ") (x1);;
+print_string "   ";;
+let () = List.iter (printf "%d ") (x2);;
+print_string "\n";;
+
+
 
 
 let polacz x1 x2 =
@@ -34,7 +60,20 @@ let polacz x1 x2 =
 in polacz2 [] x1 x2 0;;
 
 let res3 = polacz [1;2;3] [9;3;2];;
+let () = List.iter (printf "%d ") (res3);;
+print_string "\n";;
 
 
+let res3 = polacz [] [1;3;2];;
+let () = List.iter (printf "%d ") (res3);;
+print_string "\n";;
+
+
+let res3 = polacz [1] [9;3;2;3;4;4];;
+let () = List.iter (printf "%d ") (res3);;
+print_string "\n";;
+
+
+let res3 = polacz [] [];;
 let () = List.iter (printf "%d ") (res3);;
 print_string "\n";;
